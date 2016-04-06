8ce8f7c9837d56f5082390b667af784f82e03a48
Andrew Baxter <ahbaxter@outlook.com>
2016-04-06 14:50:37 -0400

    Started adding Json.NET

--- Derby.Designer.vb
+++ Derby.Designer.vb
@@ -24,16 +24,33 @@ Partial Class Derby
     Private Sub InitializeComponent()
         Me.tabBehavior = New System.Windows.Forms.TabControl()
         Me.pageRegister = New System.Windows.Forms.TabPage()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.lblID = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.lblRegisterRacer = New System.Windows.Forms.Label()
-        Me.lblRegisterManual = New System.Windows.Forms.Label()
+        Me.lblRegisterHistory = New System.Windows.Forms.Label()
+        Me.txtRegisterHistory = New System.Windows.Forms.TextBox()
+        Me.btnRegisterAdd = New System.Windows.Forms.Button()
+        Me.txtRegisterNumber = New System.Windows.Forms.TextBox()
+        Me.lblRegisterNumber = New System.Windows.Forms.Label()
+        Me.txtRegisterName = New System.Windows.Forms.TextBox()
+        Me.lblRegisterName = New System.Windows.Forms.Label()
         Me.pageTourney = New System.Windows.Forms.TabPage()
         Me.pageRace = New System.Windows.Forms.TabPage()
+        Me.grpRacesNow = New System.Windows.Forms.GroupBox()
+        Me.dudRacesPort = New System.Windows.Forms.DomainUpDown()
+        Me.lblRacePort = New System.Windows.Forms.Label()
         Me.pageResults = New System.Windows.Forms.TabPage()
+        Me.grpRacesUpNext = New System.Windows.Forms.GroupBox()
+        Me.grpRacesHole = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.gridRacesCurrent = New System.Windows.Forms.DataGridView()
+        Me.Lane = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Racer = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Rank = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TimeToFinish = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.btnRacesNext = New System.Windows.Forms.Button()
         Me.tabBehavior.SuspendLayout()
         Me.pageRegister.SuspendLayout()
+        Me.pageRace.SuspendLayout()
+        Me.grpRacesNow.SuspendLayout()
+        CType(Me.gridRacesCurrent, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'tabBehavior
@@ -52,11 +69,13 @@ Partial Class Derby
         'pageRegister
         '
         Me.pageRegister.AllowDrop = True
-        Me.pageRegister.Controls.Add(Me.TextBox2)
-        Me.pageRegister.Controls.Add(Me.lblID)
-        Me.pageRegister.Controls.Add(Me.TextBox1)
-        Me.pageRegister.Controls.Add(Me.lblRegisterRacer)
-        Me.pageRegister.Controls.Add(Me.lblRegisterManual)
+        Me.pageRegister.Controls.Add(Me.lblRegisterHistory)
+        Me.pageRegister.Controls.Add(Me.txtRegisterHistory)
+        Me.pageRegister.Controls.Add(Me.btnRegisterAdd)
+        Me.pageRegister.Controls.Add(Me.txtRegisterNumber)
+        Me.pageRegister.Controls.Add(Me.lblRegisterNumber)
+        Me.pageRegister.Controls.Add(Me.txtRegisterName)
+        Me.pageRegister.Controls.Add(Me.lblRegisterName)
         Me.pageRegister.Location = New System.Drawing.Point(4, 22)
         Me.pageRegister.Name = "pageRegister"
         Me.pageRegister.Padding = New System.Windows.Forms.Padding(3)
@@ -65,47 +84,64 @@ Partial Class Derby
         Me.pageRegister.Text = "Register Cars"
         Me.pageRegister.UseVisualStyleBackColor = True
         '
-        'TextBox2
+        'lblRegisterHistory
         '
-        Me.TextBox2.Location = New System.Drawing.Point(51, 89)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(140, 20)
-        Me.TextBox2.TabIndex = 8
+        Me.lblRegisterHistory.AutoSize = True
+        Me.lblRegisterHistory.Location = New System.Drawing.Point(21, 86)
+        Me.lblRegisterHistory.Name = "lblRegisterHistory"
+        Me.lblRegisterHistory.Size = New System.Drawing.Size(42, 13)
+        Me.lblRegisterHistory.TabIndex = 7
+        Me.lblRegisterHistory.Text = "History:"
         '
-        'lblID
+        'txtRegisterHistory
         '
-        Me.lblID.AutoSize = True
-        Me.lblID.Location = New System.Drawing.Point(6, 92)
-        Me.lblID.Name = "lblID"
-        Me.lblID.Size = New System.Drawing.Size(21, 13)
-        Me.lblID.TabIndex = 7
-        Me.lblID.Text = "ID:"
+        Me.txtRegisterHistory.Location = New System.Drawing.Point(23, 104)
+        Me.txtRegisterHistory.Multiline = True
+        Me.txtRegisterHistory.Name = "txtRegisterHistory"
+        Me.txtRegisterHistory.ReadOnly = True
+        Me.txtRegisterHistory.Size = New System.Drawing.Size(627, 267)
+        Me.txtRegisterHistory.TabIndex = 6
         '
-        'TextBox1
+        'btnRegisterAdd
         '
-        Me.TextBox1.Location = New System.Drawing.Point(51, 54)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(140, 20)
-        Me.TextBox1.TabIndex = 3
+        Me.btnRegisterAdd.Location = New System.Drawing.Point(220, 20)
+        Me.btnRegisterAdd.Name = "btnRegisterAdd"
+        Me.btnRegisterAdd.Size = New System.Drawing.Size(65, 52)
+        Me.btnRegisterAdd.TabIndex = 5
+        Me.btnRegisterAdd.Text = "Add"
+        Me.btnRegisterAdd.UseVisualStyleBackColor = True
         '
-        'lblRegisterRacer
+        'txtRegisterNumber
         '
-        Me.lblRegisterRacer.AutoSize = True
-        Me.lblRegisterRacer.Location = New System.Drawing.Point(6, 57)
-        Me.lblRegisterRacer.Name = "lblRegisterRacer"
-        Me.lblRegisterRacer.Size = New System.Drawing.Size(39, 13)
-        Me.lblRegisterRacer.TabIndex = 1
-        Me.lblRegisterRacer.Text = "Racer:"
+        Me.txtRegisterNumber.Location = New System.Drawing.Point(74, 20)
+        Me.txtRegisterNumber.Name = "txtRegisterNumber"
+        Me.txtRegisterNumber.Size = New System.Drawing.Size(140, 20)
+        Me.txtRegisterNumber.TabIndex = 2
         '
-        'lblRegisterManual
+        'lblRegisterNumber
         '
-        Me.lblRegisterManual.AutoSize = True
-        Me.lblRegisterManual.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.lblRegisterManual.Location = New System.Drawing.Point(28, 34)
-        Me.lblRegisterManual.Name = "lblRegisterManual"
-        Me.lblRegisterManual.Size = New System.Drawing.Size(139, 17)
-        Me.lblRegisterManual.TabIndex = 0
-        Me.lblRegisterManual.Text = "Enter Cars Manually:"
+        Me.lblRegisterNumber.AutoSize = True
+        Me.lblRegisterNumber.Location = New System.Drawing.Point(21, 23)
+        Me.lblRegisterNumber.Name = "lblRegisterNumber"
+        Me.lblRegisterNumber.Size = New System.Drawing.Size(47, 13)
+        Me.lblRegisterNumber.TabIndex = 1
+        Me.lblRegisterNumber.Text = "Number:"
+        '
+        'txtRegisterName
+        '
+        Me.txtRegisterName.Location = New System.Drawing.Point(74, 52)
+        Me.txtRegisterName.Name = "txtRegisterName"
+        Me.txtRegisterName.Size = New System.Drawing.Size(140, 20)
+        Me.txtRegisterName.TabIndex = 4
+        '
+        'lblRegisterName
+        '
+        Me.lblRegisterName.AutoSize = True
+        Me.lblRegisterName.Location = New System.Drawing.Point(21, 55)
+        Me.lblRegisterName.Name = "lblRegisterName"
+        Me.lblRegisterName.Size = New System.Drawing.Size(38, 13)
+        Me.lblRegisterName.TabIndex = 3
+        Me.lblRegisterName.Text = "Name:"
         '
         'pageTourney
         '
@@ -119,6 +155,9 @@ Partial Class Derby
         '
         'pageRace
         '
+        Me.pageRace.Controls.Add(Me.grpRacesHole)
+        Me.pageRace.Controls.Add(Me.grpRacesNow)
+        Me.pageRace.Controls.Add(Me.grpRacesUpNext)
         Me.pageRace.Location = New System.Drawing.Point(4, 22)
         Me.pageRace.Name = "pageRace"
         Me.pageRace.Padding = New System.Windows.Forms.Padding(3)
@@ -126,6 +165,38 @@ Partial Class Derby
         Me.pageRace.TabIndex = 2
         Me.pageRace.Text = "Run Races"
         Me.pageRace.UseVisualStyleBackColor = True
+        '
+        'grpRacesNow
+        '
+        Me.grpRacesNow.Controls.Add(Me.btnRacesNext)
+        Me.grpRacesNow.Controls.Add(Me.gridRacesCurrent)
+        Me.grpRacesNow.Controls.Add(Me.Label1)
+        Me.grpRacesNow.Controls.Add(Me.dudRacesPort)
+        Me.grpRacesNow.Controls.Add(Me.lblRacePort)
+        Me.grpRacesNow.Location = New System.Drawing.Point(3, 6)
+        Me.grpRacesNow.Name = "grpRacesNow"
+        Me.grpRacesNow.Size = New System.Drawing.Size(663, 277)
+        Me.grpRacesNow.TabIndex = 0
+        Me.grpRacesNow.TabStop = False
+        Me.grpRacesNow.Text = "Current Race:"
+        '
+        'dudRacesPort
+        '
+        Me.dudRacesPort.Location = New System.Drawing.Point(594, 278)
+        Me.dudRacesPort.Name = "dudRacesPort"
+        Me.dudRacesPort.ReadOnly = True
+        Me.dudRacesPort.Size = New System.Drawing.Size(63, 20)
+        Me.dudRacesPort.TabIndex = 7
+        Me.dudRacesPort.Text = "None"
+        '
+        'lblRacePort
+        '
+        Me.lblRacePort.AutoSize = True
+        Me.lblRacePort.Location = New System.Drawing.Point(499, 280)
+        Me.lblRacePort.Name = "lblRacePort"
+        Me.lblRacePort.Size = New System.Drawing.Size(89, 13)
+        Me.lblRacePort.TabIndex = 6
+        Me.lblRacePort.Text = "Listening on Port:"
         '
         'pageResults
         '
@@ -137,6 +208,81 @@ Partial Class Derby
         Me.pageResults.Text = "Results"
         Me.pageResults.UseVisualStyleBackColor = True
         '
+        'grpRacesUpNext
+        '
+        Me.grpRacesUpNext.Location = New System.Drawing.Point(7, 289)
+        Me.grpRacesUpNext.Name = "grpRacesUpNext"
+        Me.grpRacesUpNext.Size = New System.Drawing.Size(329, 96)
+        Me.grpRacesUpNext.TabIndex = 1
+        Me.grpRacesUpNext.TabStop = False
+        Me.grpRacesUpNext.Text = "Up Next:"
+        '
+        'grpRacesHole
+        '
+        Me.grpRacesHole.Location = New System.Drawing.Point(342, 289)
+        Me.grpRacesHole.Name = "grpRacesHole"
+        Me.grpRacesHole.Size = New System.Drawing.Size(324, 96)
+        Me.grpRacesHole.TabIndex = 2
+        Me.grpRacesHole.TabStop = False
+        Me.grpRacesHole.Text = "In The Hole:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 28)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Label1"
+        '
+        'gridRacesCurrent
+        '
+        Me.gridRacesCurrent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.gridRacesCurrent.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Lane, Me.Racer, Me.Rank, Me.TimeToFinish})
+        Me.gridRacesCurrent.Dock = System.Windows.Forms.DockStyle.Top
+        Me.gridRacesCurrent.Location = New System.Drawing.Point(3, 16)
+        Me.gridRacesCurrent.MinimumSize = New System.Drawing.Size(0, 216)
+        Me.gridRacesCurrent.Name = "gridRacesCurrent"
+        Me.gridRacesCurrent.ReadOnly = True
+        Me.gridRacesCurrent.Size = New System.Drawing.Size(657, 216)
+        Me.gridRacesCurrent.TabIndex = 9
+        '
+        'Lane
+        '
+        Me.Lane.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells
+        Me.Lane.HeaderText = "Lane"
+        Me.Lane.Name = "Lane"
+        Me.Lane.ReadOnly = True
+        Me.Lane.Width = 56
+        '
+        'Racer
+        '
+        Me.Racer.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
+        Me.Racer.HeaderText = "Racer"
+        Me.Racer.Name = "Racer"
+        Me.Racer.ReadOnly = True
+        '
+        'Rank
+        '
+        Me.Rank.HeaderText = "Rank"
+        Me.Rank.Name = "Rank"
+        Me.Rank.ReadOnly = True
+        '
+        'TimeToFinish
+        '
+        Me.TimeToFinish.HeaderText = "Time to Finish"
+        Me.TimeToFinish.Name = "TimeToFinish"
+        Me.TimeToFinish.ReadOnly = True
+        '
+        'btnRacesNext
+        '
+        Me.btnRacesNext.Location = New System.Drawing.Point(556, 238)
+        Me.btnRacesNext.Name = "btnRacesNext"
+        Me.btnRacesNext.Size = New System.Drawing.Size(101, 33)
+        Me.btnRacesNext.TabIndex = 10
+        Me.btnRacesNext.Text = "Next Race"
+        Me.btnRacesNext.UseVisualStyleBackColor = True
+        '
         'Derby
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -147,10 +293,14 @@ Partial Class Derby
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.HelpButton = True
         Me.Name = "Derby"
-        Me.Text = "Derby Jockey"
+        Me.Text = "Derby"
         Me.tabBehavior.ResumeLayout(False)
         Me.pageRegister.ResumeLayout(False)
         Me.pageRegister.PerformLayout()
+        Me.pageRace.ResumeLayout(False)
+        Me.grpRacesNow.ResumeLayout(False)
+        Me.grpRacesNow.PerformLayout()
+        CType(Me.gridRacesCurrent, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
@@ -160,9 +310,23 @@ Partial Class Derby
     Friend WithEvents pageRace As TabPage
     Friend WithEvents pageResults As TabPage
     Friend WithEvents pageRegister As TabPage
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents lblID As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents lblRegisterRacer As Label
-    Friend WithEvents lblRegisterManual As Label
+    Friend WithEvents txtRegisterNumber As TextBox
+    Friend WithEvents lblRegisterNumber As Label
+    Friend WithEvents txtRegisterName As TextBox
+    Friend WithEvents lblRegisterName As Label
+    Friend WithEvents btnRegisterAdd As Button
+    Friend WithEvents lblRegisterHistory As Label
+    Friend WithEvents txtRegisterHistory As TextBox
+    Friend WithEvents grpRacesNow As GroupBox
+    Friend WithEvents dudRacesPort As DomainUpDown
+    Friend WithEvents lblRacePort As Label
+    Friend WithEvents grpRacesUpNext As GroupBox
+    Friend WithEvents grpRacesHole As GroupBox
+    Friend WithEvents gridRacesCurrent As DataGridView
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Lane As DataGridViewTextBoxColumn
+    Friend WithEvents Racer As DataGridViewTextBoxColumn
+    Friend WithEvents Rank As DataGridViewTextBoxColumn
+    Friend WithEvents TimeToFinish As DataGridViewTextBoxColumn
+    Friend WithEvents btnRacesNext As Button
 End Class
--- Derby.vb
+++ Derby.vb
@@ -1,35 +1,44 @@
-﻿'File:    Derby.vb
-'Author:  Andrew Baxter
-'Date:    March 7, 2016
-'Purpose: Operate a Pinewood Derby tournament, scheduling races and interpreting results from an Arduino unit
+﻿' File:    Derby.vb
+' Author:  Andrew Baxter
+' Date:    March 7, 2016
+' Purpose: Operate a Pinewood Derby tournament, scheduling races and interpreting results from an Arduino unit
 
 Imports System.IO.Ports
+Imports Newtonsoft.Json
+Imports Newtonsoft.Json.Linq
 
 Public Class Derby
+
+
+
     Private Const buffsize As UInteger = 8
 
-    Private WithEvents serial As New System.IO.Ports.SerialPort("COM1", 9600)
+    Private WithEvents serial As System.IO.Ports.SerialPort = Nothing ' As New System.IO.Ports.SerialPort("COM1", 9600)
     Private thread As New System.Threading.Thread(AddressOf KickoffRead)
-    Private readbuff(buffsize) As Byte 'Four bytes for car ID, four bytes for time taken
+    Private readbuff(buffsize) As Byte ' Four bytes for car ID, four bytes for time taken
 
-    Private carsFinished As New List(Of CarFinished)
+    Private carsFinished As New List(Of CarFinishedPacket)
     Private listMutex As Object
 
     Private Sub HandleData(data As Byte(), size As Integer)
-        If (size < 8) Then
+        If (size < buffsize) Then ' Wait for a complete packet. Not sure if the port holds on to the old data though...
             Return
         End If
 
         SyncLock listMutex
-            carsFinished.Add(New CarFinished(data))
+            carsFinished.Add(New CarFinishedPacket(data))
         End SyncLock
     End Sub
 
     Private Sub HandleReadError(ex As Exception)
-        MessageBox.Show(ex.ToString())
+        If dudRacesPort.SelectedItem Is serial.PortName And serial.IsOpen Then ' Don't report an error if they're in the middle of switching ports
+            MessageBox.Show("Error reading data:" + vbNewLine + ex.ToString())
+        Else
+            Debug.WriteLine(ex.ToString())
+        End If
     End Sub
 
-    Private Sub KickoffRead()
+    Private Sub KickoffRead() ' Begins an asynchronous read operation on the serial port
         serial.BaseStream.BeginRead(readbuff, 0, buffsize,
                                     Sub(ar As IAsyncResult)
                                         Try
@@ -44,23 +53,90 @@ Public Class Derby
                                     Nothing)
     End Sub
 
-    Private Sub Derby_Load(sender As Object, e As EventArgs) Handles MyBase.Load 'Move to a "Start Listening" button click from pageRace
-        serial.Open()
-        KickoffRead()
+    Private Sub Derby_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim ports As New Collection
+        dudRacesPort.Items.Add("None")
+        dudRacesPort.Items.Add("COM1")
+        dudRacesPort.Items.Add("COM2")
+        dudRacesPort.Items.Add("COM3")
+        dudRacesPort.Items.Add("COM4")
+
+        If Not IO.File.Exists("./data/racers.json") Then
+            If Not IO.Directory.Exists("./data") Then
+                IO.Directory.CreateDirectory("./data")
+            End If
+            Dim temp As IO.FileStream = IO.File.Create("./data/racers.json")
+            temp.Close()
+        End If
+        racer_file = New IO.StreamWriter(IO.File.Open("./data/racers.json", IO.FileMode.Open And IO.FileMode.Append))
+
     End Sub
 
-    Private Sub Derby_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing 'Move to a "Stop Listening" button clink from pageRace
-        serial.Close()
+    Private Sub Derby_Close(sender As Object, e As EventArgs) Handles MyBase.Closed
+        racer_file.Close()
+    End Sub
+
+    Private Sub dudRacesPort_SelectedItemChanged(sender As Object, e As EventArgs) Handles dudRacesPort.SelectedItemChanged
+
+        If Not serial Is Nothing AndAlso serial.IsOpen Then
+            serial.Close()
+        End If
+
+        If Not (dudRacesPort.SelectedItem Is Nothing OrElse dudRacesPort.SelectedItem Is "None") Then
+            Try
+                serial = New SerialPort(dudRacesPort.SelectedItem, 9600)
+                serial.Open()
+                KickoffRead()
+            Catch ex As Exception
+                MessageBox.Show("Failed to open communications on " + dudRacesPort.SelectedItem)
+                Return
+            End Try
+        End If
+
+    End Sub
+
+    Private txtReg_lastNum As Integer
+    Private Sub txtRegisterNumber_TextChanged(sender As Object, e As EventArgs) Handles txtRegisterNumber.TextChanged
+        Dim test_result As Integer
+        If Integer.TryParse(txtRegisterNumber.Text, test_result) Then
+            txtReg_lastNum = test_result
+        Else
+            ' txtRegisterNumber.Text is not an integer, and is invalid
+            ' Set it to whatever was in in the last time it passed this test
+            txtRegisterNumber.Text = txtReg_lastNum.ToString()
+        End If
+    End Sub
+
+
+    Private racer_file As IO.StreamWriter
+    Private serializer As New JsonSerializer
+    Private Sub btnRegisterAdd_Click(sender As Object, e As EventArgs) Handles btnRegisterAdd.Click
+        If Not (String.IsNullOrEmpty(txtRegisterNumber.Text) Or String.IsNullOrEmpty(txtRegisterName.Text)) Then
+            Dim entry As New RacerEntry
+            entry.id = Integer.Parse(txtRegisterNumber.Text)
+            entry.name = txtRegisterName.Text
+            serializer.Serialize(racer_file, entry)
+
+            txtReg_lastNum += 1
+            txtRegisterNumber.Text = txtReg_lastNum.ToString()
+        Else
+            MessageBox.Show("Insufficient registration data")
+        End If
     End Sub
 End Class
 
+Public Class RacerEntry
+    Public id As String
+    Public name As String
+End Class
 
-Public Class CarFinished
+Public Class CarFinishedPacket
     Public Sub New(data As Byte())
         id = BitConverter.ToInt32(data, 0)
         time_ms = BitConverter.ToInt32(data, 4)
     End Sub
 
-    Public Property id As UInt32
-    Public Property time_ms As UInt32
+    ' These may need to be Integers instead of Int32s
+    Public id As Int32
+    Public time_ms As Int32
 End Class
